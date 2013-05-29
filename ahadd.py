dfad93548718b71ed6fd7b08f9b273ae78053b92
Alexander Gude <gude@physics.umn.edu>
2013-05-29 12:00:51 -0500

    Better conform to pep8

--- ahadd.py
+++ ahadd.py
@@ -16,20 +16,22 @@
 #  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 
-from optparse import OptionParser # Command line parsing
-from tempfile import mkdtemp # Secure methods of generating random directories
+from optparse import OptionParser  # Command line parsing
+from tempfile import mkdtemp  # Secure methods of generating random directories
 from random import choice
-from string import ascii_uppercase,digits,ascii_lowercase
-from sys import exit # Cleanly exit program
-from subprocess import call # Access external programs
-from os import listdir,remove,devnull
+from string import ascii_uppercase, digits, ascii_lowercase
+from sys import exit  # Cleanly exit program
+from subprocess import call  # Access external programs
+from os import listdir, remove, devnull
 from os.path import isfile
-from shutil import copy2,rmtree
+from shutil import copy2, rmtree
+
 
 ## Help functions
-def listdirwithpath(dir):
+def listdirwithpath(d):
     """ Return a list of files with their path """
-    return [ dir+'/'+f for f in listdir(dir) ]
+    return [d + '/' + f for f in listdir(d)]
+
 
 ## Hadd class
 class hadd:
@@ -51,17 +53,19 @@ class hadd:
 
     def run(self):
         """ Combine files by looping over them """
-        if not self.quite: print "Combining files"
+        if not self.quite:
+            print "Combining files"
 
         i = 0
-        infiles = list(self.infiles) # deep copies the list
+        infiles = list(self.infiles)  # deep copies the list
         while True:
-            currentWriteDir = mkdtemp(prefix=str(i)+"_", dir=self.tmpdir) + '/'
+            currentWriteDir = mkdtemp(prefix=str(i) + "_", dir=self.tmpdir) + '/'
             self.__haddMultiple(currentWriteDir, infiles)
             currentReadDir = currentWriteDir
             infiles = listdirwithpath(currentReadDir)
             if len(infiles) == 1:
-                if not self.quite: print "Copying final file:",infiles[0],"-->",self.outfile
+                if not self.quite:
+                    print "Copying final file:", infiles[0], "-->", self.outfile
                 copy2(infiles[0], self.outfile)
                 break
             else:
@@ -71,26 +75,27 @@ class hadd:
     def __printInAndOut(self):
         """ Print the input files, and output file """
         if not self.quite:
-            print "Output file:",out_file
+            print "Output file:", out_file
             print "Input files:"
             for f in in_files:
-                print "\t",f
-            print "Number of files to hadd at once:",self.natonce
+                print "\t", f
+            print "Number of files to hadd at once:", self.natonce
 
     def __checkOutFile(self):
         """ Check if the output file exists, if so exit if we don't force
         overwrite """
         if not self.force_overwrite and isfile(self.outfile):
-            print "Output file already exists! File:",self.outfile
-            exit(1) # Other error
+            print "Output file already exists! File:", self.outfile
+            exit(1)  # Other error
         elif self.force_overwrite and isfile(self.outfile):
-            if not self.quite: print "Output file already exists; it will be overwriten! File:",self.outfile
+            if not self.quite:
+                print "Output file already exists; it will be overwriten! File:", self.outfile
 
     def __cleanup(self):
         """ Clean up intermediate files """
         if not self.save:
             rmtree(self.tmpdir)
-        exit(0) # Normal exit
+        exit(0)  # Normal exit
 
     def __haddMultiple(self, writeDir, inFiles):
         """ Given a list of inFiles, and a write dir, hadds the files and saves
@@ -109,46 +114,49 @@ class hadd:
 
     def __getRandomRootName(self, dir):
         """ Return a random file name """
-        chars = ascii_uppercase+digits+ascii_lowercase
+        chars = ascii_uppercase + digits + ascii_lowercase
         ran = []
         for x in xrange(6):
             ran.append(choice(chars))
         num = self.counter
         self.counter += 1
-        return dir + '/input_%i_'%(num) + ''.join(ran) + ".root"
+        return dir + '/input_%i_' % (num) + ''.join(ran) + ".root"
 
-    def __setTmpDir(self,tmpdir):
+    def __setTmpDir(self, tmpdir):
         """ Set up a temporary directory to hold intermediate files """
-        if self.verbose: print "Making temporary directory:",
+        if self.verbose:
+            print "Making temporary directory:",
 
         if tmpdir == None:
             self.tmpdir = mkdtemp(prefix="ahadd_") + '/'
         else:
-            self.tmpdir = mkdtemp(prefix="ahadd_",dir=options.tmp_dir) + '/'
+            self.tmpdir = mkdtemp(prefix="ahadd_", dir=options.tmp_dir) + '/'
 
-        if self.verbose: print self.tmpdir
+        if self.verbose:
+            print self.tmpdir
 
     def __hadd(self, outfile, infiles):
         """ Call hadd """
         args = ["hadd", outfile] + infiles
-        if self.verbose: 
+        if self.verbose:
             print "Calling hadd"
-            print "\tOutput:",outfile
+            print "\tOutput:", outfile
             print "\tInput:"
             for f in infiles:
-                print "\t\t",f
+                print "\t\t", f
 
         if self.vverbose:
             return (call(args), outfile)
         else:
             return (call(args, stdout=open(devnull, 'wb')), outfile)
 
+
 ##### START OF CODE
 """ Allows command line options to be parsed. Called first to in order to let functions use them.  """
 
 usage = "usage: %prog [Options] outputfile inputfiles"
 version = "%prog Version 1.1.1\n\nCopyright (C) 2012 Alexander Gude - gude@physics.umn.edu\nThis is free software.  You may redistribute copies of it under the terms of\nthe GNU General Public License <http://www.gnu.org/licenses/gpl.html>.\nThere is NO WARRANTY, to the extent permitted by law.\n\nWritten by Alexander Gude."
-parser = OptionParser(usage=usage,version=version)
+parser = OptionParser(usage=usage, version=version)
 parser.add_option("-n", "--n-files-at-once", action="store", type="int", dest="natonce", default=20, help="combine this many files at one time [defualt 4]")
 parser.add_option("-t", "--temp-dir", action="store", type="string", dest="tmp_dir", default=None, help="location to store temporary intermediate files")
 parser.add_option("-s", "--save-temp", action="store_true", dest="save_tmp", default=False, help="save temporary files, otherwise they are cleaned up when the program exits [default false]")
@@ -171,7 +179,7 @@ in_files = []
 out_file = None
 if len(args) <= 2:
     print "Not enough arguments on the command line. Exiting."
-    exit(2) # Not enough commands
+    exit(2)  # Not enough commands
 else:
     out_file = args[0]
     in_files = args[1:]
@@ -179,7 +187,7 @@ else:
 ## Check that the number of files to hadd each step is sane
 if options.natonce <= 1:
     print "Requested to hadd 1 or fewer files per iteration; this will never converge."
-    exit(1) # Other error
+    exit(1)  # Other error
 
 ## Set up and run hadd
 h = hadd(out_file, in_files, options.tmp_dir, options.verbose, options.vverbose, options.quite, options.force_overwrite, options.save_tmp, options.natonce)
